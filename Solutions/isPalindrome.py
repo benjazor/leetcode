e161c27fac3aa8afa0a4aeac79107168c3092baa
benjazor <benjazor@gmail.com>
2021-01-29 12:36:34 +0100

    Solved isPalindrome

---
+++ Solutions/isPalindrome.py
@@ -0,0 +1,14 @@
+# NAME : Palindrome Number
+# LINK : https://leetcode.com/problems/palindrome-number/
+# DATE : 29/01/2021
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        y = 0
+        while x > y:
+            y = y * 10 + x % 10
+            x //= 10
+        return x == y or x == y // 10
