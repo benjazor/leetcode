5b2d6d7f2dcbd6701a5edb9b15da488b41e1a9fe
benjazor <benjazor@gmail.com>
2021-02-20 10:39:17 +0100

    Solved romanToInt

--- Solutions/romanToInt.py
+++ Solutions/romanToInt.py
@@ -1,6 +1,6 @@
 # NAME : Roman to Integer
 # LINK : https://leetcode.com/problems/roman-to-integer/
-# DATE : 29/01/2021
+# DATE : 29/01/2021, 20/02/2021
 
 
 class Solution:
@@ -17,4 +17,26 @@ class Solution:
             else:
                 result -= n
 
+        return result
+
+
+class Solution2:
+    def romanToInt(self, s: str) -> int:
+        romans = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        result = 0
+        maxSeen = s[-1]
+        for i in range(1, len(s) + 1):
+            if romans[s[-i]] >= romans[maxSeen]:
+                result += romans[s[-i]]
+                maxSeen = s[-i]
+            else:
+                result -= romans[s[-i]]
         return result
